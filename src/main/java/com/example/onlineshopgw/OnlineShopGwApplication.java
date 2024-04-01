ba49a54b5908c4c7f409cc52cb3527bd205a974e
Emre Işıldaklı <suleymanemre62@gmail.com>
2024-04-01 21:06:40 +0300

    Created gateway for online shop

---
+++ src/main/java/com/example/onlineshopgw/OnlineShopGwApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineshopgw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopGwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopGwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlineshopgw/OnlineShopGwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineshopgw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopGwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
